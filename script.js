f9b1abb35e67f3ad3ee52eb4d1fea4fbaae73cf9
Daniel Hawn <danieljhawn@gmail.com>
2019-11-10 22:38:44 -0600

    marginal progress

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-queryURL = "api.openweathermap.org/data/2.5/weather?q=`${city}`,`${country}`&APPID=a01fee2d80b973ab3b18ce1990905e04"
-city = "Austin";
-country = "us";
+function searchCity(city){
+    let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=a01fee2d80b973ab3b18ce1990905e04&units=imperial`
+    console.log(queryURL)
+    $.ajax({url: queryURL, method: "GET"})
+    .then(function(searchData) {
+        console.log(searchData.main.temp);
+        uvIndex(searchData.coord.lat, searchData.coord.lon);
+        localStorage.setItem('lastCity', JSON.stringify(searchData));
+        // $("#cityLast").text(JSON.stringify(localStorage.lastCity));
+        $("#cityLast").text(localStorage.lastCity.cod);
+        $("#tempLast").text(localStorage.lastCity.main.temp);
 
-function searchCity(city, queryURL){
+    })
+}
+
+function forecast(city){
+    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=a01fee2d80b973ab3b18ce1990905e04&units=imperial`
+    console.log(queryURL)
     $.ajax({url: queryURL, method: "GET"})
     .then(function(searchData) {
         console.log(searchData);
     })
 }
 
+function uvIndex(lat, lon){
+    let uvi = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=a01fee2d80b973ab3b18ce1990905e04&units=imperial`
+}
 $('#searchBtn').on('click', function() {
-    alert("test")
-    searchCity();
+    let city = $("#searchForm").val()
+    console.log(city);
+    searchCity(city);
+    forecast(city);
 })
+
+// function populateMain {
+//     $(".mainBox").prepend(localStorage.lastCity);
+// }
