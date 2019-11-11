30707f3014873962f39c9d94f71413eb21628c15
Daniel Hawn <danieljhawn@gmail.com>
2019-11-11 15:29:27 -0600

    adds localStorage functionality

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-function searchCity(city){
+//this variable stores the last search as an object in localStorage
+var lastCity = JSON.parse(localStorage.getItem('lastCity'));
+
+
+function searchCity(city) {
     let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=a01fee2d80b973ab3b18ce1990905e04&units=imperial`
-    console.log(queryURL)
-    $.ajax({url: queryURL, method: "GET"})
-    .then(function(searchData) {
-        console.log(searchData.main.temp);
-        uvIndex(searchData.coord.lat, searchData.coord.lon);
-        localStorage.setItem('lastCity', JSON.stringify(searchData));
-        // $("#cityLast").text(JSON.stringify(localStorage.lastCity));
-        $("#cityLast").text(localStorage.lastCity.cod);
-        $("#tempLast").text(localStorage.lastCity.main.temp);
-
-    })
+        // console.log(queryURL)
+    $.ajax({ url: queryURL, method: "GET" })
+        .then(function(searchData) {
+            console.log(searchData);
+            console.log(searchData.name);
+            uvIndex(searchData.coord.lat, searchData.coord.lon);
+            localStorage.setItem('lastCity', JSON.stringify(searchData));
+        })
 }
 
-function forecast(city){
+function forecast(city) {
     let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=a01fee2d80b973ab3b18ce1990905e04&units=imperial`
-    console.log(queryURL)
-    $.ajax({url: queryURL, method: "GET"})
-    .then(function(searchData) {
-        console.log(searchData);
-    })
+        // console.log(queryURL)
+    $.ajax({ url: queryURL, method: "GET" })
+        .then(function(searchData) {
+            console.log(searchData);
+        })
 }
 
-function uvIndex(lat, lon){
+function uvIndex(lat, lon) {
     let uvi = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=a01fee2d80b973ab3b18ce1990905e04&units=imperial`
 }
 $('#searchBtn').on('click', function() {
@@ -30,8 +31,14 @@ $('#searchBtn').on('click', function() {
     console.log(city);
     searchCity(city);
     forecast(city);
+    populateMain();
 })
 
-// function populateMain {
-//     $(".mainBox").prepend(localStorage.lastCity);
-// }
+function populateMain() {
+    $("#cityLast").text(lastCity.name);
+    $("#tempLast").text(lastCity.main.temp + " Farenheight");
+    $("#humiLast").text("Humidity: " + lastCity.main.humidity + " %");
+    $("#speedLast").text(lastCity.wind.speed + " MPH");
+
+}
+populateMain();
